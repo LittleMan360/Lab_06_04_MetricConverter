4a0aa46bf142bba3f3c12debbe7271dc0266d558
Dalton <dalton.m.towe@gmail.com>
2023-02-19 16:29:45 -0500

    added base code before tests

--- src/metric.java
+++ src/metric.java
@@ -6,13 +6,13 @@ public class metric {
 
         //variables
         double meters = 0;
-        double inch = 0;
-        double feet = 0;
-        double miles = 0;
-        String wrongInput = "";
+        double inches;
+        double feet;
+        double miles;
+        String wrongInput;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter measurement in meters " );
+        System.out.println("Enter measurement in meters ");
         if (in.hasNextDouble())
         {
             meters = in.nextDouble();
@@ -20,12 +20,21 @@ public class metric {
         else
         {
             wrongInput = in.nextLine();
+            in.nextLine();
             System.out.println("You said the measurement was " + wrongInput);
             System.out.println("Run the program again and enter correct measurement in numbers only");
             System.exit(0);//terminates program
         }
 
+        //figured 1 meter = 39.36 inches
+        inches = meters * 39.36;
+        feet = meters * 3.28;
+        miles = meters / 1609.344;
 
+
+        System.out.println(meters + " meters = " + inches + " inches");
+        System.out.println(meters + " meters = " + feet + " feet");
+        System.out.println(meters + " meters = " + miles + " miles");
 
 
     }
