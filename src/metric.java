0940f0c2112a58457e826fb0569780b97942ce0c
Dalton <dalton.m.towe@gmail.com>
2023-02-19 16:21:06 -0500

    added variables and input

--- src/metric.java
+++ src/metric.java
@@ -4,7 +4,29 @@ public class metric {
     public static void main(String[] args)
     {
 
-       
+        //variables
+        double meters = 0;
+        double inch = 0;
+        double feet = 0;
+        double miles = 0;
+        String wrongInput = "";
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter measurement in meters " );
+        if (in.hasNextDouble())
+        {
+            meters = in.nextDouble();
+        }
+        else
+        {
+            wrongInput = in.nextLine();
+            System.out.println("You said the measurement was " + wrongInput);
+            System.out.println("Run the program again and enter correct measurement in numbers only");
+            System.exit(0);//terminates program
+        }
+
+
+
 
     }
 }
